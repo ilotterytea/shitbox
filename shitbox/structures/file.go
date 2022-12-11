88c10616e8eafadd71acfd0522a67a146b7c8287
NotDankEnough <hatesociety@proton.me>
2022-12-12 00:07:45 +0600

    file struct

---
+++ shitbox/structures/file.go
@@ -0,0 +1,15 @@
+package structures
+
+// Shitbox's file data.
+type File struct {
+	// Literal ID.
+	LiteralId string
+	// MIME type of the file.
+	MIME string
+	// Extension of the file.
+	Ext string
+	// Secret key. It is used to perform some operations with the file.
+	SecretKey string
+	// A timestamp of when the file was uploaded.
+	Timestamp int64
+}
