9d12838172b456f3c7fa551c6c0f3bcd1fd98af4
NotDankEnough <hatesociety@proton.me>
2022-12-12 00:36:12 +0600

    Create a folder for uploads

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	handlers "github.com/ilotterytea/shitbox/shitbox/handlers"
 )
 
 func main() {
+	_, err := os.Stat("./directory")
+	if err != nil {
+		if os.IsNotExist(err) {
+			os.Mkdir("./uploaded", os.FileMode(0777))
+		} else {
+			log.Fatal(err)
+		}
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
