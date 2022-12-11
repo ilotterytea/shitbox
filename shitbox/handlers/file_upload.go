8d657aa025a9dd1923c6628646d7d8ee171bd747
NotDankEnough <hatesociety@proton.me>
2022-12-12 00:09:48 +0600

    Return JSON + use rand hex token for name

--- shitbox/handlers/file_upload.go
+++ shitbox/handlers/file_upload.go
@@ -2,8 +2,13 @@ package shitbox
 
 import (
 	"fmt"
+	"mime"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/ilotterytea/shitbox/shitbox/structures"
+	"github.com/ilotterytea/shitbox/shitbox/utils"
 )
 
 // Handler for requests to upload a file.
@@ -13,5 +18,36 @@ func HandleFileUpload(c *fiber.Ctx) error {
 		return err
 	}
 
-	return c.SaveFile(file, fmt.Sprintf("./uploaded/%s", file.Filename))
+	id, _ := utils.RandomHexToken(3)
+	key, _ := utils.RandomHexToken(8)
+	mime, err := mime.ExtensionsByType(file.Header.Get("Content-Type"))
+	if err != nil {
+		return err
+	}
+	if len(mime) == 0 {
+		c.SendStatus(500)
+		return c.JSON(fiber.Map{
+			"status":  500,
+			"message": "The MIME type of this file was not found by the server. The file upload was stopped.",
+			"file":    nil,
+		})
+	}
+
+	ifile := structures.File{
+		LiteralId: id,
+		MIME:      file.Header.Get("Content-Type"),
+		Ext:       mime[len(mime)-1],
+		Timestamp: time.Now().Unix(),
+		SecretKey: key,
+	}
+
+	c.SaveFile(file, fmt.Sprintf("./uploaded/%s%s", id, mime[len(mime)-1]))
+
+	return c.JSON(fiber.Map{
+		"status":  200,
+		"message": "Success!",
+		"meta":    ifile,
+		"get":     fmt.Sprintf("%s/%s%s", c.BaseURL(), ifile.LiteralId, ifile.Ext),
+		"delete":  fmt.Sprintf("%s/%s%s?key=%s", c.BaseURL(), ifile.LiteralId, ifile.Ext, ifile.SecretKey),
+	})
 }
